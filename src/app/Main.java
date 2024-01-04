3d703aab1d7d72c22452e9f8978709ae3004fe76
yandenysov <chickenbuster1337@gmail.com>
2024-01-04 12:32:10 +0200

    Initial commit. Mile -> Kilometer converter implementation

---
+++ src/app/Main.java
@@ -0,0 +1,21 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter distance in miles which will be converted into kilometers: ");
+        double miles = scanner.nextDouble();
+        double kilometers = convertMilesToKilometers(miles);
+        printConversionResult(miles, kilometers);
+    }
+
+    private static double convertMilesToKilometers(double miles) {
+        return miles * 1.609344;
+    }
+
+    private static void printConversionResult(double miles, double kilometers) {
+        System.out.println(miles + " miles = " + kilometers + " kilometers");
+    }
+}
